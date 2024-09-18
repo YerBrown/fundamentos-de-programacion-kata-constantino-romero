72f21cc3e70de7e24ec5b745629f7c91627d5e57
YerBrown <yerbrown98@gmail.com>
2024-09-18 09:26:53 +0200

    Añadido estructura de añadir la pregunta y respuestas al html

--- scripts.js
+++ scripts.js
@@ -1,41 +1,41 @@
 const pregunta = {
-    titulo: '¿A quién ha doblado más veces Constantino Romero?',
-    respuestas: [
-        {
-            label: "Clint Eastwood",
-            id: "clint-eastwood",
-            name: "actor",
-            value: "clint-eastwood"
-        },
-        {
-            label: "James Earl",
-            id: "james-earl",
-            name: "actor",
-            value: "james-earl"
-        },
-        {
-            label: "Roger Moore",
-            id: "roger-moore",
-            name: "actor",
-            value: "roger-moore"
-        },
-        {
-            label: "William Shatner",
-            id: "william-shatner",
-            name: "actor",
-            value: "william-shatner"
-        },
-        {
-            label: "Arnold Schwarzenegger",
-            id: "arnold-schwarzenegger",
-            name: "actor",
-            value: "arnold-schwarzenegger"
-        }
-    ]
-}
+  titulo: "¿A quién ha doblado más veces Constantino Romero?",
+  respuestas: [
+    {
+      label: "Clint Eastwood",
+      id: "clint-eastwood",
+      name: "actor",
+      value: "clint-eastwood",
+    },
+    {
+      label: "James Earl",
+      id: "james-earl",
+      name: "actor",
+      value: "james-earl",
+    },
+    {
+      label: "Roger Moore",
+      id: "roger-moore",
+      name: "actor",
+      value: "roger-moore",
+    },
+    {
+      label: "William Shatner",
+      id: "william-shatner",
+      name: "actor",
+      value: "william-shatner",
+    },
+    {
+      label: "Arnold Schwarzenegger",
+      id: "arnold-schwarzenegger",
+      name: "actor",
+      value: "arnold-schwarzenegger",
+    },
+  ],
+};
 
 function imprimePregunta(pregunta) {
-  const newHTML = "";
+  let newHTML = "";
 
   newHTML += imprimeTitulo(pregunta);
   newHTML += imprimeTodasLasRespuestas(pregunta);
@@ -44,23 +44,31 @@ function imprimePregunta(pregunta) {
 }
 
 function imprimeTitulo(pregunta) {
-  // Put your code here
+  return "<p>" + pregunta.titulo + "</p>";
 }
 
 function imprimeTodasLasRespuestas(pregunta) {
-  // Put your code here
+  let todasPreguntas = "";
+  for (let i = 0; i < pregunta.respuestas.length; i++) {
+    todasPreguntas += imprimeUnaRespuesta(pregunta.respuestas[i]);
+  }
+  return todasPreguntas;
 }
 
 function imprimeUnaRespuesta(respuesta) {
-  // Put your code here
+  let textoRespuesta = "";
+  textoRespuesta += imprimeLabel(respuesta) + "\n";
+  textoRespuesta += imprimeInput(respuesta) + "\n";
+  return textoRespuesta;
 }
 
 function imprimeLabel(respuesta) {
-  // Put your code here
+  return `<label for="${respuesta.id}">${respuesta.label}</label>`;
 }
 
 function imprimeInput(respuesta) {
-  // Put your code here
+  return `<input id="${respuesta.id}" name="${respuesta.name}" type="radio" value="${respuesta.value}">`;
 }
 
-document.getElementById("contenedorPreguntas").innerHTML = imprimePregunta(pregunta);
+document.getElementById("contenedorPreguntas").innerHTML =
+  imprimePregunta(pregunta);
